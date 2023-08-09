be8c10c35121f358a77fde6645c124be6202962e
AllanSS2 <allansampa49@gmail.com>
2023-08-09 14:17:01 -0300

    Sexto commit

--- dbLaboratorio.sql
+++ dbLaboratorio.sql
@@ -55,4 +55,22 @@ select * from PRODUTOS where nome not like '%y%';
 
 --6. Exibir todos os produtos que se iniciem com nome 'MO' e tenham como tipo as letras 'MA' 
 
-select * from produtos where nome like 'mo%' and tipo like '%ma%'
+select * from produtos where nome like 'mo%' and tipo like '%ma%';
+
+
+-- Laboratorio 2
+-- 1. Escreva uma consulta que exiba os dados da tabela PRODUTOS na forma crescente pelo campo NOME. 
+
+select * from produtos order by nome asc;
+
+-- 2. Escreva uma consulta que exiba o NOME dos produtos ordenado de forma decrescente e o VALOR de forma crescente
+
+select * from produtos order by nome desc, valor asc;
+
+-- 3. Escreva uma consulta que exiba os três produtos mais caros
+
+select * from produtos order by valor desc limit 3;
+
+-- 4. Escreva uma consulta que exiba o VALOR do produto mais barato.
+
+select * from produtos order by valor asc limit 1;
