04150d2631ec87a3f6414def20ae5c07a4953795
AllanSS2 <allansampa49@gmail.com>
2023-08-24 17:09:23 -0300

    Décimo commit

--- dbMercadoSA.sql
+++ dbMercadoSA.sql
@@ -93,3 +93,43 @@ select * from tbFornecedores;
 select * from tbUsuarios;
 select * from tbProdutos;
 select * from tbVendas;
+
+-- inner join
+select usu.nome as 'Nome do usúario', func.nome as 'Nome do funcionário', func.email as 'Email do funcionário' from tbUsuarios as usu inner join tbFuncionarios as func on usu.codFunc = func.codFunc;
+
+select tbProdutos.descricao as 'Descrição do produto', tbFornecedores.nome as 'Nome do Fornecedor', tbFornecedores.cnpj as 'CNPJ do fornecedor' from tbProdutos inner join tbFornecedores on tbProdutos.codForn = tbFornecedores.codForn;
+
+select cli.nome as 'Nome do CLiente', cli.email as 'Email do Cliente', cli.telCel as 'Telefone do Cliente', vend.dataVenda as 'Data da Venda', .vend.valorTotal as 'Valor total' from tbVendas as vend inner join tbClientes as cli on vend.codCli = cli.codCli;
+
+-- Para 2 tabelas
+select cli.nome, cli.email, prod.descricao, vend.dataVenda from tbVendas as vend inner join tbClientes as cli on vend.codCli = cli.codCli inner join tbProdutos as prod on vend.codProd = prod.codProd;
+
+select cli.nome, cli.email, prod.descricao, vend.dataVenda from tbVendas as vend inner join tbClientes as cli on vend.codCli = cli.codCli inner join tbProdutos as prod on vend.codProd = prod.codProd where vend.codVenda = 1;
+
+select cli.nome, cli.email, prod.descricao, vend.dataVenda from tbVendas as vend inner join tbClientes as cli on vend.codCli = cli.codCli inner join tbProdutos as prod on vend.codProd = prod.codProd where prod.descricao like '%c%';
+
+-- Perguntando para tabela de usuários
+-- nome do usuário
+-- nome do funcionário
+-- data da venda
+-- quantidade vendida
+-- valor total da venda
+select usu.nome, func.nome, vend.dataVenda, vend.quantidade, vend.valorTotal from tbUsuarios as usu inner join tbFuncionarios as func on usu.codFunc = func.codFunc inner join tbVendas as vend on usu.codUsu = vend.codUsu;
+
+-- Perguntando para tabela de clientes
+-- nome do cliente
+-- data da venda
+-- nome do produto
+-- nome do fornecedor
+select cli.nome as 'Nome do Cliente', vend.dataVenda as 'Data da Venda', prod.descricao as 'Nome do Produto', forn.nome as 'Nome do Fornecedor' from tbClientes as cli inner join tbVendas as vend on cli.codCli = vend.codCli inner join tbProdutos as prod on vend.codProd = prod.codProd inner join tbFornecedores as forn on prod.codForn = forn.codForn;
+
+-- Perguntando para tabela de funcionarios
+-- nome do funcionário
+-- nome do usuário
+-- data da venda
+-- valor total
+-- nome do produto
+-- data da entrada
+-- nome do fornecedor
+-- cnpj do fornecedor
+select func.nome as 'Nome do funcionário', usu.nome as 'Nome do usúario', vend.dataVenda as 'Data da venda', vend.valorTotal as 'Valor total', prod.descricao as 'Nome do produto', prod.dataEntrada as 'Data da entrada', forn.nome as 'Nome do fornecedor', forn.cnpj as 'CNPJ do fornecedor' from tbFuncionarios as func inner join tbUsuarios as usu on func.codFunc = usu.codFunc inner join tbVendas as vend on usu.codUsu = vend.codUsu inner join tbProdutos as prod on vend.codProd = prod.codProd inner join tbFornecedores as forn on prod.codForn = forn.codForn;
